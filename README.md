e8b68ef5f94c021721d2dba828855da148713570
Hritwik <hritwiksinghal@outlook.in>
2020-12-12 01:37:56 +0530

    update comments, readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # NLP
+
+## Team Name: Spartans
+### Members:
+Hritwk
+
+Sameer
+
+Vani
+
+## Goal
+Our goal is to get the basic knowledge of how the algorithms work in real world and how to implement them. We will analyse the words we took from two books, apply pre-processing, and analyse frequency distribution of tokens, create word cloud from tokens with stopwords and without stopwords. We also have to analyse the relationship between word length and frequency and POS tag the words using the PennTreebank tag set. We are using NLTK python library for all the data pre-processing, analysing and POS tagging (PennTreebank tag set comes pre-loaded in NLTK python library) since it will give us very accurate results and will enable us to work with such large text book corpus
+
+## Data description
+
+The dataset we used are two books “The adventure of Sherlock Holmes” and “Alice in the wonderland”. We can get them for free. The input will be the words from the books and output will be according to problem statements. For POS tagging we have used Penn Treebank tag set and we have used NLTK library in python for processing and analysing this data. 
--- start_NLP_project.py
+++ start_NLP_project.py
@@ -1,6 +1,8 @@
-# This is a NLP Python project. Uses NLTK Library (pip install nltk)
-# we first have to download nltk data, use "nltk.download()"
-
+""""
+This is a NLP Python project. Uses NLTK Library (pip install nltk)
+We first have to download nltk data, use "nltk.download()"
+"""
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
@@ -13,22 +15,26 @@ import pos_tag
 lemmatizer = WordNetLemmatizer()
 
 
-# function to import book file and return a list
-# the list contains lines of book as its elements
 def import_book(book_file_name: str):
-    #  "sig" in "utf-8-sig" is the abbreviation of "signature" (i.e. signature utf-8 file).
-    #  Using utf-8-sig to read a file will treat BOM as file info. instead of a string
-    # from https://stackoverflow.com/questions/57152985/what-is-the-difference-between-utf-8-and-utf-8-sig
+    """function to import book file and return a list, the list contains lines of book as its elements."""
 
+    """
+    "sig" in "utf-8-sig" is the abbreviation of "signature" (i.e. signature utf-8 file).
+    Using utf-8-sig to read a file will treat BOM as file info. instead of a string
+    from https://stackoverflow.com/questions/57152985/what-is-the-difference-between-utf-8-and-utf-8-sig
+    """
     with open(book_file_name, 'r+', encoding='utf-8-sig') as temp:
         book = [line.strip() for line in temp.readlines() if line.strip()]
 
     return book
 
 
-# takes a book as string, tokenize it, then lemmetize those tokens
-# then join those lemmas to form new book and return both the new book and lemmas
 def tokenize_and_lemmatization(book):
+    """
+    Takes a book as string, tokenize it, then lemmetize those tokens
+    then join those lemmas to form new book and return both the new book and lemmas.
+    """
+
     # tokenize the book after lower-casing the sentences
     final_list = []
     word_tokens = word_tokenize(book.lower())
@@ -45,8 +51,9 @@ def tokenize_and_lemmatization(book):
     return new_book, lemmas
 
 
-# Process, generate tokens and do lemmatization
 def pre_processing_book(book):
+    """Process, generate tokens and do lemmatization."""
+
     # applying all pre-processing and storing result in a string
     new_book = pre_process.start(book)
 
@@ -56,13 +63,15 @@ def pre_processing_book(book):
     return tokens, new_book
 
 
-# analyze frequency distribution of tokens
 def analyze_freq_distribution_of_tokens(tokens, book_file_name):
+    """Analyze frequency distribution of tokens."""
+
     freq_dist_tokens.start(tokens, book_file_name)
 
 
-# function to generate word Clouds
 def generate_word_cloud(words, book_file_name):
+    """Function to generate word Clouds."""
+
     # without stopwords removal
     word_cloud.start(words, book_file_name, stopwords_flag=0)
 
@@ -70,8 +79,9 @@ def generate_word_cloud(words, book_file_name):
     word_cloud.start(words, book_file_name, stopwords_flag=1)
 
 
-# count freq of each token and return it in a Dict
 def count_freq_of_each_token(tokens: list):
+    """Count freq of each token and return it in a Dict."""
+
     freq = {}
     for word in tokens:
         freq[word] = freq.get(word, 0) + 1
@@ -80,8 +90,9 @@ def count_freq_of_each_token(tokens: list):
     return {k: v for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)}
 
 
-# to get relationship between the word length and frequency
 def get_relationship_between_the_word_length_and_frequency(tokens: list, book_file_name):
+    """To get relationship between the word length and frequency."""
+
     # count freq of each token
     freq = count_freq_of_each_token(tokens)
 
@@ -89,12 +100,23 @@ def get_relationship_between_the_word_length_and_frequency(tokens: list, book_fi
     relation_word_len_freq.start(freq, book_file_name)
 
 
-# do POS_tagging and Get the distribution of various tags
 def do_pos_tag_and_get_dist_tags(tokens, book_file_name):
+    """Do POS_tagging and Get the distribution of various tags."""
+
     pos_tag.start(tokens, book_file_name)
 
 
+def get_nouns():
+    pass
+
+
 if __name__ == '__main__':
+    """This function runs first."""
+
+    # uncomment below line if you want to download nltk data.
+    # nltk.download('all')
+
+
     # name of the files of book1 and book2 as stored on our hard drive
     book_file_name_list = [
         '0_alice.book',
